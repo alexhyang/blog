f434acacc2209e5301b3bf35cbd6144760b26467
Alex Yang <alex.hyang.dev@gmail.com>
2023-08-20 15:45:36 -0700

    upgrade to tailwindcss v3

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// themeDir two levels above dir of current module
+module.exports = {
+  plugins: {
+    // 'postcss-import': { path: [themeDir] },
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
